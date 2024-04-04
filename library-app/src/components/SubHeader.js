7bddd27762262cd3ad22793b96522452d293a66d
AparnaVjn <149356560+AparnaVjn@users.noreply.github.com>
2024-04-04 08:29:23 +0530

    modified:   library-app/src/components/SubHeader.js
    Align search bar and add book button to the right

--- library-app/src/components/SubHeader.js
+++ library-app/src/components/SubHeader.js
@@ -4,18 +4,23 @@ import React from 'react'
 function SubHeader() {
   return (
     <div>
-        <div className="container d-flex flex-wrap justify-content-center">
-            <div className=''>
+        <div className=" d-flex m-5 justify-content-between" style={{background:"#509091"}}>
+            <div className=" d-flex">
+            <div className='p-2'>
             <form className="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto">
           <input type="search" className="form-control" placeholder="Search..." aria-label="Search"/>          
         </form>
-        <button type="button" className="btn btn-primary">Add Book</button>
+        </div>
+       
+        <div className='p-2'>
+        <button type="button" className="btn btn-secondary ms-2">Search</button>
+         </div>
             </div>
+       
         
 
-        <div className="text-end">
-          
-          <button type="button" className="btn btn-primary">Add Book</button>
+        <div className="p-2 ml-10" >
+              <button type="button" className="btn btn-secondary">Add Book</button>
         </div>
       </div>
     </div>
